518c37841a417b8a2b57c19577e43e16e8d36fd4
luisraffo <108698836+LuisRaffo@users.noreply.github.com>
2022-08-20 18:56:29 -0300

    Entrega 1
    Modificado cambiado el archivo archivo index.html a houes.html, login.html a index.html. Creado pantalla de login y lista de productos

---
+++ js/login.js
@@ -0,0 +1,18 @@
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const boton = document.querySelector ("button");
+
+    boton.addEventListener("click",(evento)=>{
+        evento.preventDefault();
+        const Usuario = document.querySelector("#Usuario");
+        const Contraseña = document.querySelector("#Contraseña");
+        if ( Usuario.value == "" || Contraseña.value == ""){
+            showAlertError()
+        } else{
+            window.location.href = "http://127.0.0.1:5500/home.html";
+        }
+    })
+})
